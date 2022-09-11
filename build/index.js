55c477882109007e647cbe8d00bd78504e71b940
Minskoleopc <minskoleopc@gmail.com>
2022-09-11 16:47:32 +0530

    dayone

---
+++ build/index.js
@@ -0,0 +1,21 @@
+"use strict";
+//console.log('hello world')
+// let x:number = 10
+// console.log(x)
+// let firstName:string = "chinmay"
+// console.log(firstName)
+// program1
+function add(x, y) {
+    console.log(x + y);
+}
+add(5, 6);
+// program
+let firstName = "chinmay";
+let x = 10;
+let y = true;
+let j = {
+    firstName: "chinmay",
+    lastName: "deshpande"
+};
+let x1 = [1, 2, 3];
+let x2 = ["chinmay", "shrish"];
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+//console.log('hello world')
+
+// let x:number = 10
+// console.log(x)
+
+// let firstName:string = "chinmay"
+// console.log(firstName)
+
+// program1
+
+function add( x:number, y:number){
+    console.log(x+y) 
+}
+add(5,6)
+
+// program
+
+let firstName:string = "chinmay"
+let x:number = 10
+let y:boolean = true
+let j:object = {
+    firstName:"chinmay",
+    lastName:"deshpande"
+}
+let x1 :number[] =[1,2,3] 
+let x2 :string[] = ["chinmay","shrish"]
+
+
+
